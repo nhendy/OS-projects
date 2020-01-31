240287986fffe32be2db6eb3bbc4dfdacb4c419c
Damini K Rijhwani <drijhwan@ecegrid-thin1.ecn.purdue.edu>
2020-01-31 11:53:15 -0500

    sync.c  implmenetation of condCreate

--- lab2/os/synch.c
+++ lab2/os/synch.c
@@ -11,28 +11,28 @@
 #include "synch.h"
 #include "queue.h"
 
-static Sem sems[MAX_SEMS]; 	// All semaphores in the system
-static Lock locks[MAX_LOCKS];   // All locks in the system
+static Sem sems[MAX_SEMS];     // All semaphores in the system
+static Lock locks[MAX_LOCKS];  // All locks in the system
 
-extern struct PCB *currentPCB; 
+extern struct PCB *currentPCB;
 //----------------------------------------------------------------------
 //	SynchModuleInit
 //
 //	Initializes the synchronization primitives: the semaphores
 //----------------------------------------------------------------------
 int SynchModuleInit() {
-  int i; // Loop Index variable
-  dbprintf ('p', "SynchModuleInit: Entering SynchModuleInit\n");
-  for(i=0; i<MAX_SEMS; i++) {
+  int i;  // Loop Index variable
+  dbprintf('p', "SynchModuleInit: Entering SynchModuleInit\n");
+  for (i = 0; i < MAX_SEMS; i++) {
     sems[i].inuse = 0;
   }
-  for(i=0; i<MAX_LOCKS; i++) {
+  for (i = 0; i < MAX_LOCKS; i++) {
     // Your stuff for initializing locks goes here
   }
-  for(i=0; i<MAX_CONDS; i++) {
+  for (i = 0; i < MAX_CONDS; i++) {
     // Your stuff for initializing Condition variables goes here
   }
-  dbprintf ('p', "SynchModuleInit: Leaving SynchModuleInit\n");
+  dbprintf('p', "SynchModuleInit: Leaving SynchModuleInit\n");
   return SYNC_SUCCESS;
 }
 
@@ -44,10 +44,12 @@ int SynchModuleInit() {
 //	initting the process queue and setting the counter.
 //
 //----------------------------------------------------------------------
-int SemInit (Sem *sem, int count) {
+int SemInit(Sem *sem, int count) {
   if (!sem) return SYNC_FAIL;
-  if (AQueueInit (&sem->waiting) != QUEUE_SUCCESS) {
-    printf("FATAL ERROR: could not initialize semaphore waiting queue in SemInit!\n");
+  if (AQueueInit(&sem->waiting) != QUEUE_SUCCESS) {
+    printf(
+        "FATAL ERROR: could not initialize semaphore waiting queue in "
+        "SemInit!\n");
     exitsim();
   }
   sem->count = count;
@@ -67,20 +69,19 @@ sem_t SemCreate(int count) {
 
   // grabbing a semaphore should be an atomic operation
   intrval = DisableIntrs();
-  for(sem=0; sem<MAX_SEMS; sem++) {
-    if(sems[sem].inuse==0) {
+  for (sem = 0; sem < MAX_SEMS; sem++) {
+    if (sems[sem].inuse == 0) {
       sems[sem].inuse = 1;
       break;
     }
   }
   RestoreIntrs(intrval);
-  if(sem==MAX_SEMS) return SYNC_FAIL;
+  if (sem == MAX_SEMS) return SYNC_FAIL;
 
   if (SemInit(&sems[sem], count) != SYNC_SUCCESS) return SYNC_FAIL;
   return sem;
 }
 
-
 //----------------------------------------------------------------------
 //
 //	SemWait
@@ -92,41 +93,47 @@ sem_t SemCreate(int count) {
 //      turned on before going to sleep.
 //
 //----------------------------------------------------------------------
-int SemWait (Sem *sem) {
-  Link	*l;
-  int		intrval;
-    
+int SemWait(Sem *sem) {
+  Link *l;
+  int intrval;
+
   if (!sem) return SYNC_FAIL;
 
-  intrval = DisableIntrs ();
-  dbprintf ('I', "SemWait: Old interrupt value was 0x%x.\n", intrval);
-  dbprintf ('s', "SemWait: Proc %d waiting on sem %d, count=%d.\n", GetCurrentPid(), (int)(sem-sems), sem->count);
+  intrval = DisableIntrs();
+  dbprintf('I', "SemWait: Old interrupt value was 0x%x.\n", intrval);
+  dbprintf('s', "SemWait: Proc %d waiting on sem %d, count=%d.\n",
+           GetCurrentPid(), (int)(sem - sems), sem->count);
   if (sem->count <= 0) {
     dbprintf('s', "SemWait: putting process %d to sleep\n", GetCurrentPid());
-    if ((l = AQueueAllocLink ((void *)currentPCB)) == NULL) {
-      printf("FATAL ERROR: could not allocate link for semaphore queue in SemWait!\n");
+    if ((l = AQueueAllocLink((void *)currentPCB)) == NULL) {
+      printf(
+          "FATAL ERROR: could not allocate link for semaphore queue in "
+          "SemWait!\n");
       exitsim();
     }
-    if (AQueueInsertLast (&sem->waiting, l) != QUEUE_SUCCESS) {
-      printf("FATAL ERROR: could not insert new link into semaphore waiting queue in SemWait!\n");
+    if (AQueueInsertLast(&sem->waiting, l) != QUEUE_SUCCESS) {
+      printf(
+          "FATAL ERROR: could not insert new link into semaphore waiting queue "
+          "in SemWait!\n");
       exitsim();
     }
     ProcessSleep();
   } else {
-    dbprintf('s', "SemWait: Proc %d granted permission to continue by sem %d\n", GetCurrentPid(), (int)(sem-sems));
+    dbprintf('s', "SemWait: Proc %d granted permission to continue by sem %d\n",
+             GetCurrentPid(), (int)(sem - sems));
   }
-  sem->count--; // Decrement intenal counter
-  RestoreIntrs (intrval);
+  sem->count--;  // Decrement intenal counter
+  RestoreIntrs(intrval);
   return SYNC_SUCCESS;
 }
 
 int SemHandleWait(sem_t sem) {
   if (sem < 0) return SYNC_FAIL;
   if (sem >= MAX_SEMS) return SYNC_FAIL;
-  if (!sems[sem].inuse)    return SYNC_FAIL;
+  if (!sems[sem].inuse) return SYNC_FAIL;
   return SemWait(&sems[sem]);
 }
-
+
 //----------------------------------------------------------------------
 //
 //	SemSignal
@@ -135,44 +142,48 @@ int SemHandleWait(sem_t sem) {
 //	_OSC_.
 //
 //----------------------------------------------------------------------
-int SemSignal (Sem *sem) {
+int SemSignal(Sem *sem) {
   Link *l;
-  int	intrs;
+  int intrs;
   PCB *pcb;
 
   if (!sem) return SYNC_FAIL;
 
-  intrs = DisableIntrs ();
-  dbprintf ('s', "SemSignal: Process %d Signalling on sem %d, count=%d.\n", GetCurrentPid(), (int)(sem-sems), sem->count);
+  intrs = DisableIntrs();
+  dbprintf('s', "SemSignal: Process %d Signalling on sem %d, count=%d.\n",
+           GetCurrentPid(), (int)(sem - sems), sem->count);
   // Increment internal counter before checking value
   sem->count++;
-  if (sem->count > 0) { // check if there is a process to wake up
-    if (!AQueueEmpty(&sem->waiting)) { // there is a process to wake up
+  if (sem->count > 0) {  // check if there is a process to wake up
+    if (!AQueueEmpty(&sem->waiting)) {  // there is a process to wake up
       l = AQueueFirst(&sem->waiting);
       pcb = (PCB *)AQueueObject(l);
-      if (AQueueRemove(&l) != QUEUE_SUCCESS) { 
-        printf("FATAL ERROR: could not remove link from semaphore queue in SemSignal!\n");
+      if (AQueueRemove(&l) != QUEUE_SUCCESS) {
+        printf(
+            "FATAL ERROR: could not remove link from semaphore queue in "
+            "SemSignal!\n");
         exitsim();
       }
-      dbprintf ('s', "SemSignal: Waking up PID %d.\n", (int)(GetPidFromAddress(pcb)));
-      ProcessWakeup (pcb);
+      dbprintf('s', "SemSignal: Waking up PID %d.\n",
+               (int)(GetPidFromAddress(pcb)));
+      ProcessWakeup(pcb);
     }
   }
-  RestoreIntrs (intrs);
+  RestoreIntrs(intrs);
   return SYNC_SUCCESS;
 }
 
 int SemHandleSignal(sem_t sem) {
   if (sem < 0) return SYNC_FAIL;
   if (sem >= MAX_SEMS) return SYNC_FAIL;
-  if (!sems[sem].inuse)    return SYNC_FAIL;
+  if (!sems[sem].inuse) return SYNC_FAIL;
   return SemSignal(&sems[sem]);
 }
 
 //-----------------------------------------------------------------------
 //	LockCreate
 //
-//	LockCreate grabs a lock from the systeme-wide pool of locks and 
+//	LockCreate grabs a lock from the systeme-wide pool of locks and
 //	initializes it.
 //	It also sets the inuse flag of the lock to indicate that the lock is
 //	being used by a process. It returns a unique id for the lock. All the
@@ -188,14 +199,14 @@ lock_t LockCreate() {
 
   // grabbing a lock should be an atomic operation
   intrval = DisableIntrs();
-  for(l=0; l<MAX_LOCKS; l++) {
-    if(locks[l].inuse==0) {
+  for (l = 0; l < MAX_LOCKS; l++) {
+    if (locks[l].inuse == 0) {
       locks[l].inuse = 1;
       break;
     }
   }
   RestoreIntrs(intrval);
-  if(l==MAX_LOCKS) return SYNC_FAIL;
+  if (l == MAX_LOCKS) return SYNC_FAIL;
 
   if (LockInit(&locks[l]) != SYNC_SUCCESS) return SYNC_FAIL;
   return l;
@@ -203,8 +214,9 @@ lock_t LockCreate() {
 
 int LockInit(Lock *l) {
   if (!l) return SYNC_FAIL;
-  if (AQueueInit (&l->waiting) != QUEUE_SUCCESS) {
-    printf("FATAL ERROR: could not initialize lock waiting queue in LockInit!\n");
+  if (AQueueInit(&l->waiting) != QUEUE_SUCCESS) {
+    printf(
+        "FATAL ERROR: could not initialize lock waiting queue in LockInit!\n");
     exitsim();
   }
   l->pid = -1;
@@ -214,42 +226,50 @@ int LockInit(Lock *l) {
 //---------------------------------------------------------------------------
 //	LockHandleAcquire
 //
-//	This routine acquires a lock given its handle. The handle must be a 
-//	valid handle for this routine to succeed. In that case this routine 
+//	This routine acquires a lock given its handle. The handle must be a
+//	valid handle for this routine to succeed. In that case this routine
 //	returns SYNC_FAIL. Otherwise the routine returns SYNC_SUCCESS.
 //
 //---------------------------------------------------------------------------
 int LockAcquire(Lock *k) {
-  Link	*l;
-  int		intrval;
-    
+  Link *l;
+  int intrval;
+
   if (!k) return SYNC_FAIL;
 
   // Locks are atomic
-  intrval = DisableIntrs ();
-  dbprintf ('I', "LockAcquire: Old interrupt value was 0x%x.\n", intrval);
+  intrval = DisableIntrs();
+  dbprintf('I', "LockAcquire: Old interrupt value was 0x%x.\n", intrval);
 
   // Check to see if the current process owns the lock
   if (k->pid == GetCurrentPid()) {
-    dbprintf('s', "LockAcquire: Proc %d already owns lock %d\n", GetCurrentPid(), (int)(k-locks));
+    dbprintf('s', "LockAcquire: Proc %d already owns lock %d\n",
+             GetCurrentPid(), (int)(k - locks));
     RestoreIntrs(intrval);
     return SYNC_SUCCESS;
   }
 
-  dbprintf ('s', "LockAcquire: Proc %d asking for lock %d.\n", GetCurrentPid(), (int)(k-locks));
-  if (k->pid >= 0) { // Lock is already in use by another process
-    dbprintf('s', "LockAcquire: putting process %d to sleep\n", GetCurrentPid());
-    if ((l = AQueueAllocLink ((void *)currentPCB)) == NULL) {
-      printf("FATAL ERROR: could not allocate link for lock queue in LockAcquire!\n");
+  dbprintf('s', "LockAcquire: Proc %d asking for lock %d.\n", GetCurrentPid(),
+           (int)(k - locks));
+  if (k->pid >= 0) {  // Lock is already in use by another process
+    dbprintf('s', "LockAcquire: putting process %d to sleep\n",
+             GetCurrentPid());
+    if ((l = AQueueAllocLink((void *)currentPCB)) == NULL) {
+      printf(
+          "FATAL ERROR: could not allocate link for lock queue in "
+          "LockAcquire!\n");
       exitsim();
     }
-    if (AQueueInsertLast (&k->waiting, l) != QUEUE_SUCCESS) {
-      printf("FATAL ERROR: could not insert new link into lock waiting queue in LockAcquire!\n");
+    if (AQueueInsertLast(&k->waiting, l) != QUEUE_SUCCESS) {
+      printf(
+          "FATAL ERROR: could not insert new link into lock waiting queue in "
+          "LockAcquire!\n");
       exitsim();
     }
     ProcessSleep();
   } else {
-    dbprintf('s', "LockAcquire: lock is available, assigning to proc %d\n", GetCurrentPid());
+    dbprintf('s', "LockAcquire: lock is available, assigning to proc %d\n",
+             GetCurrentPid());
     k->pid = GetCurrentPid();
   }
   RestoreIntrs(intrval);
@@ -259,7 +279,7 @@ int LockAcquire(Lock *k) {
 int LockHandleAcquire(lock_t lock) {
   if (lock < 0) return SYNC_FAIL;
   if (lock >= MAX_LOCKS) return SYNC_FAIL;
-  if (!locks[lock].inuse)    return SYNC_FAIL;
+  if (!locks[lock].inuse) return SYNC_FAIL;
   return LockAcquire(&locks[lock]);
 }
 
@@ -267,45 +287,52 @@ int LockHandleAcquire(lock_t lock) {
 //	LockHandleRelease
 //
 //	This procedure releases the unique lock described by the handle. It
-//	first checks whether the lock is a valid lock. If not, it returns SYNC_FAIL.
+//	first checks whether the lock is a valid lock. If not, it returns
+// SYNC_FAIL.
 //	If the lock is a valid lock, it should check whether the calling
-//	process actually holds the lock. If not it returns SYNC_FAIL. Otherwise it
+//	process actually holds the lock. If not it returns SYNC_FAIL. Otherwise
+// it
 //	releases the lock, and returns SYNC_SUCCESS.
 //---------------------------------------------------------------------------
 int LockRelease(Lock *k) {
   Link *l;
-  int	intrs;
+  int intrs;
   PCB *pcb;
 
   if (!k) return SYNC_FAIL;
 
-  intrs = DisableIntrs ();
-  dbprintf ('s', "LockRelease: Proc %d releasing lock %d.\n", GetCurrentPid(), (int)(k-locks));
+  intrs = DisableIntrs();
+  dbprintf('s', "LockRelease: Proc %d releasing lock %d.\n", GetCurrentPid(),
+           (int)(k - locks));
 
   if (k->pid != GetCurrentPid()) {
-    dbprintf('s', "LockRelease: Proc %d does not own lock %d.\n", GetCurrentPid(), (int)(k-locks));
+    dbprintf('s', "LockRelease: Proc %d does not own lock %d.\n",
+             GetCurrentPid(), (int)(k - locks));
     return SYNC_FAIL;
   }
   k->pid = -1;
-  if (!AQueueEmpty(&k->waiting)) { // there is a process to wake up
+  if (!AQueueEmpty(&k->waiting)) {  // there is a process to wake up
     l = AQueueFirst(&k->waiting);
     pcb = (PCB *)AQueueObject(l);
-    if (AQueueRemove(&l) != QUEUE_SUCCESS) { 
-      printf("FATAL ERROR: could not remove link from lock queue in LockRelease!\n");
+    if (AQueueRemove(&l) != QUEUE_SUCCESS) {
+      printf(
+          "FATAL ERROR: could not remove link from lock queue in "
+          "LockRelease!\n");
       exitsim();
     }
-    dbprintf ('s', "LockRelease: Waking up PID %d, assigning lock.\n", (int)(GetPidFromAddress(pcb)));
+    dbprintf('s', "LockRelease: Waking up PID %d, assigning lock.\n",
+             (int)(GetPidFromAddress(pcb)));
     k->pid = GetPidFromAddress(pcb);
-    ProcessWakeup (pcb);
+    ProcessWakeup(pcb);
   }
-  RestoreIntrs (intrs);
+  RestoreIntrs(intrs);
   return SYNC_SUCCESS;
 }
 
 int LockHandleRelease(lock_t lock) {
   if (lock < 0) return SYNC_FAIL;
   if (lock >= MAX_LOCKS) return SYNC_FAIL;
-  if (!locks[lock].inuse)    return SYNC_FAIL;
+  if (!locks[lock].inuse) return SYNC_FAIL;
   return LockRelease(&locks[lock]);
 }
 
@@ -316,8 +343,8 @@ int LockHandleRelease(lock_t lock) {
 //	condition variables and associates the specified lock with
 //	it. It should also initialize all the fields that need to initialized.
 //	The lock being associated should be a valid lock, which means that
-//	it should have been obtained via previous call to LockCreate(). 
-//	
+//	it should have been obtained via previous call to LockCreate().
+//
 //	If for some reason a condition variable cannot be created (no more
 //	condition variables left, or the specified lock is not a valid lock),
 //	this function should return INVALID_COND (see synch.h). Otherwise it
@@ -325,7 +352,19 @@ int LockHandleRelease(lock_t lock) {
 //--------------------------------------------------------------------------
 cond_t CondCreate(lock_t lock) {
   // Your code goes here
-  return SYNC_FAIL;
+  cond_t cond;
+  unint32 intrval;
+  if (lock == INVALID_LOCK) return SYNC_FAIL;
+  // grab condition variable
+  intrval = DisableIntrs();
+  for (cond - 0; cond < MAX_CONDS; cond++) {
+    if (conds[cond].inuse == 0) {
+      conds[cond].inuse = 1;
+      break;
+    }
+  }
+  RestoreIntrs(intrval);
+  if () return SYNC_FAIL;
 }
 
 //---------------------------------------------------------------------------
@@ -355,8 +394,6 @@ int CondHandleWait(cond_t c) {
   // Your code goes here
   return SYNC_SUCCESS;
 }
-
-
 
 //---------------------------------------------------------------------------
 //	CondHandleSignal
