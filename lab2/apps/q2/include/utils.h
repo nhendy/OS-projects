98214c5833d43cc804396b03b04c55a27f94f5e1
nhendy <nhendy@purdue.edu>
2020-02-01 22:36:51 -0500

    clean makefile

--- lab2/apps/q2/include/utils.h
+++ lab2/apps/q2/include/utils.h
@@ -1,8 +1,12 @@
 #ifndef __UTILS_HEADER__
 #define __UTILS_HEADER__
 #include "common.h"
+// Decodes command line args passed from makeprocs. Used in producer
+// and consumer to unify decoding routine.
 ConsumerProducerContext decodeArgs(const int argc,
                                    const char* const* const argv);
 
+// Clean up routine called at the end of the producer and consumer. Used
+// to signal the all_proccess_done semaphore so that makeprocs process exits.
 void cleanAndSignal(const ConsumerProducerContext ctxt);
 #endif
