aef2b9a928287644268077385e274cc126527743
Damini K Rijhwani <drijhwan@ecegrid-thin1.ecn.purdue.edu>
2020-02-10 21:05:25 -0500

    Cond struct

--- lab2/include/os/synch.h
+++ lab2/include/os/synch.h
@@ -11,20 +11,22 @@
 //	Condition variables and locks are implemented using semaphores.
 //
 
-#ifndef	_synch_h_
-#define	_synch_h_
+#ifndef _synch_h_
+#define _synch_h_
 
 #include "queue.h"
 
-#define SYNC_FAIL -1    // Used as return values from most synchronization functions.
-#define SYNC_SUCCESS 1  // Note that many functions return a handle, hence the -1 value
-                        // for failure.
+#define SYNC_FAIL \
+  -1  // Used as return values from most synchronization functions.
+#define SYNC_SUCCESS \
+  1  // Note that many functions return a handle, hence the -1 value
+     // for failure.
 
-#define MAX_SEMS	32	//Maximum 32 semaphores allowed in the system
-#define MAX_LOCKS	64	//Maximum 64 locks allowed in the system
-				//This is because condition vars also use
-				//locks from the same pool
-#define MAX_CONDS	32	//Maximum 32 conds allowed in the system
+#define MAX_SEMS 32   // Maximum 32 semaphores allowed in the system
+#define MAX_LOCKS 64  // Maximum 64 locks allowed in the system
+// This is because condition vars also use
+// locks from the same pool
+#define MAX_CONDS 32  // Maximum 32 conds allowed in the system
 
 typedef int sem_t;
 typedef int lock_t;
@@ -35,20 +37,20 @@ typedef int cond_t;
 #define INVALID_PROC -1
 #define INVALID_COND -1
 typedef struct Sem {
-    Queue	waiting;
-    int		count;
-    uint32	inuse; 		//indicates whether the semaphore is being
-    				//used by any process
+  Queue waiting;
+  int count;
+  uint32 inuse;  // indicates whether the semaphore is being
+                 // used by any process
 } Sem;
 
-int SemInit (Sem *, int);
-int SemWait (Sem *);
-int SemSignal (Sem *);
+int SemInit(Sem *, int);
+int SemWait(Sem *);
+int SemSignal(Sem *);
 
 typedef struct Lock {
-  int pid;       // PID of process holding the lock, -1 if lock is available
-  Queue waiting; // Queue of processes waiting on the lock
-  int inuse;     // Bookkeeping variable for free vs. used structures
+  int pid;        // PID of process holding the lock, -1 if lock is available
+  Queue waiting;  // Queue of processes waiting on the lock
+  int inuse;      // Bookkeeping variable for free vs. used structures
 } Lock;
 
 int LockInit(Lock *);
@@ -57,6 +59,10 @@ int LockRelease(Lock *);
 
 typedef struct Cond {
   // Your code goes here
+  int pid;  // do i need this to initialize?
+  Queue waiting;
+  uint32 inuse;
+  lock_t l;
 } Cond;
 
 int CondInit(Cond *);
@@ -75,5 +81,6 @@ cond_t CondCreate(lock_t lock);
 int CondHandleWait(cond_t cond);
 int CondHandleSignal(cond_t cond);
 int CondHandleBroadcast(cond_t cond);
+int CondBroadCast(Cond c);
 
-#endif	//_synch_h_
+#endif  //_synch_h_
