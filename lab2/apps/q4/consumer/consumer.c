62641d887899c1d05339a35e07a7dc392a536ec5
Damini K Rijhwani <drijhwan@ecegrid-thin1.ecn.purdue.edu>
2020-02-14 14:09:41 -0500

    q4 forking consumer and producer processes, and consumer process done -> need producer  to  test

--- lab2/apps/q4/consumer/consumer.c
+++ lab2/apps/q4/consumer/consumer.c
@@ -1,5 +1,58 @@
 #include "lab2-api.h"
 #include "usertraps.h"
 #include "misc.h"
+#include "circular_buffer.h"
 
-void main(int argc, , char *argv[]) {}
+void main(int argc, char *argv[]) {
+
+  CircularBuffer *circ_buff_ptr;
+  uint32 h_mem;
+
+  lock_t buffer_lock;
+  cond_t full;
+  cond_t empty;
+  sem_t sem_proc;
+  int i = 0;
+  int buffer_size;
+
+  if (argc != NUM_CMDLINE_ARGS) {
+    LOG("Incorrect number of arguments");
+    Printf("Got %d. Expected %d\n", argc, NUM_CMDLINE_ARGS);
+    Exit();
+  }
+
+  h_mem = dstrol(argv[1], NULL, 10);
+  sem_proc = dstrol(argv[1], NULL, 10);
+  buffer_lock = dstrol(argv[1], NULL, 10);
+  full = dstrol(argv[1], NULL, 10);
+  empty = dstrol(argv[1], NULL, 10);
+
+  if (circ_buff_ptr = ((CircularBuffer *)shmat(h_mem)) == NULL) {
+    Printf("Error in mapping shared memory page");
+    Exit();
+  }
+
+  for (i = 0; i < dstrlen(HELLO_WORLD_STR); ++i) {
+    if (lock_acquire(buffer_lock) == SYNC_FAIL) {
+      Printf("Could not aquire lock: %d, pid: %d", buffer_lock, getpid());
+      Exit();
+    }
+    while (circ_buff_ptr->read_index = circ_buff_ptr->write_index) {
+      cond_wait(full);
+    }
+    Printf("%c removed from buffer, pid: %d",
+           circ_buff_ptr->buffer[circ_buff_ptr->read_index], getpid());
+    circ_buff_ptr->read_index =
+        (circ_buff_ptr->read_index + 1) % BUFFER_MAX_SIZE;
+    cond_signal(empty);
+    if (lock_release(buffer_lock) == SYNC_FAIL) {
+      Printf("Could not release lock: %d, pid: %d", buffer_lock, getpid());
+      Exit();
+    }
+  }
+  Printf("consumer: PID %d is complete.\n", getpid());
+  if (sem_signal(sem_proc) == SYNC_FAIL) {
+    Printf("Bad semaphore sem_proc %d in ", sem_proc);
+    Exit();
+  }
+}
---
+++ lab2/apps/q4/makeprocs/makeprocs.c
@@ -0,0 +1,84 @@
+#include "lab2-api.h"
+#include "usertraps.h"
+#include "misc.h"
+#include "../q2/include/circular_buffer.h"
+
+void main(int argc, char *argv[]) {
+  int numprocs = 0;
+  int i;
+  char h_mem_str[10];
+  char s_procs_completed_str[10];
+  sem_t s_procs_completed;
+  uint32 h_mem;
+  CircularBuffer *circ_buffer_ptr;
+
+  cond_t full;
+  cond_t empty;
+  lock_t buffer_lock;
+
+  // command line stuff
+  char buffer_lock_str[10];
+  char full_str[10];
+  char empty_str[10];
+
+  if (argc != 2) {
+    LOG("Imcorrect argument numbers");
+    Exit();
+  }
+
+  numprocs = dstrol(argv[1], NULL, 10);
+  if ((h_mem = shmget()) == 0) {
+    Printf("ERROR: could not allocate shared memory page in ");
+    Printf(", exiting...\n");
+    Exit();
+  }
+
+  if ((circ_buffer_ptr = (CircularBuffer *)shmat(circ_buffer_ptr)) == NULL) {
+    Printf("Could not map the shared page to virtual address in ");
+    Printf(", exiting..\n");
+    Exit();
+  }
+
+  if (!initCircularBuffer(circ_buffer_ptr)) {
+    Printf("Circular buffer failed to initialize\n");
+    Exit();
+  }
+
+  if (s_procs_completed = sem_create((-(numprocs - 1)))) {
+    Printf("Failed to init all_processes_done_sem semaphore\n");
+    Exit();
+  }
+  if ((buffer_lock = lock_create()) == SYNC_FAIL) {
+    Printf("Bad lock create");
+    Exit();
+  }
+  if ((full == cond_create(buffer_lock)) == SYNC_FAIL) {
+    Printf("Bad cond_creat for buffer full\n");
+    Exit();
+  }
+  if ((empty == cond_create(buffer_lock)) == SYNC_FAIL) {
+    Printf("Bad cond_create for buffer empty\n");
+  }
+
+  ditoa(h_mem, h_mem_str);
+  ditoa(s_procs_completed, s_procs_completed_str);
+  ditoa(buffer_lock, buffer_lock_str);
+  ditoa(full, full_str);
+  ditoa(empty, empty_str);
+
+  for (i = 0; i < numprocs; i++) {
+    process_create(CONSUMER_BINARY, h_mem_str, s_procs_completed_str,
+                   buffer_lock_str, empty_str, full_str, NULL);
+  }
+  for (i = 0; i < numprocs; i++) {
+    process_create(PRODUCER_BINARY, h_mem_str, s_procs_completed_str,
+                   buffer_lock_str, empty_str, full_str, NULL);
+  }
+
+  if (sem_wait(s_procs_completed) != SYNC_SUCCESS) {
+    Printf("Bad sem_wait s_procs_completed (%d) in ", s_procs_completed);
+    Printf("\n");
+    Exit();
+  }
+  Printf("All other processes completed, exiting main process. \n");
+}
