07677649f139c9f39212fb735f8500d000dfffa6
Damini K Rijhwani <drijhwan@ecegrid-thin1.ecn.purdue.edu>
2020-04-12 13:54:39 -0400

    fork

---
+++ lab4/fork/one-level/apps/example/test_fork/test_fork.c
@@ -0,0 +1,31 @@
+#include "misc.h"
+#include "usertraps.h"
+
+void main(int argc, char *argv[]) {
+  int child_pid;
+  int x = 200;
+
+  Printf("test : testing fork pid = %d \n", getpid());
+  Printf("checking x(%d) before fork pid = %d\n", x, getpid());
+  child_pid = fork();
+  // test if child
+  if (child_pid == 0) {
+    Printf("This is child the process with pid %d \n", getpid());
+    Printf("Before x changed: pid:%d checking x(%d) after fork child pid = \n",
+           getpid(), x, child_pid);
+    // change x to see if forked properly and
+    x = 320;
+    Printf("After x changed: pid:%d checking x(%d) after fork child pid = \n",
+           getpid(), x, child_pid);
+  } else {
+    Printf("This is parent the process with pid %d \n", getpid());
+    Printf("Before x changed: pid:%d checking x(%d) after fork child pid = \n",
+           getpid(), x, child_pid);
+    // change x to see if forked properly and
+    x = 430;
+    Printf("After x changed: pid:%d checking x(%d) after fork child pid = \n",
+           getpid(), x, child_pid);
+  }
+
+  Printf("Successfully Forked!\n");
+}
