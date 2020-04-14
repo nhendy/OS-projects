4140de9490c22a5d61ad86646770658964ed204c
Damini K Rijhwani <drijhwan@ecegrid-thin1.ecn.purdue.edu>
2020-04-13 22:21:58 -0400

    testing

--- lab4/fork/one-level/apps/example/test_fork/test_fork.c
+++ lab4/fork/one-level/apps/example/test_fork/test_fork.c
@@ -3,11 +3,37 @@
 
 void main(int argc, char *argv[]) {
   int child_pid;
+  int child_pid2;
   int x = 200;
 
   Printf("Test : testing fork PID = %d \n", getpid());
   Printf("Checking x(%d) before fork PID = %d\n", x, getpid());
   child_pid = fork();
+  child_pid2 = fork();
+  // test if child
+
+  if (child_pid2 == 0) {
+    Printf("This is child 2 the process with PID %d \n", getpid());
+    Printf(
+        "Before x changed: pid:%d checking x(%d) after fork child 2 PID = %d\n",
+        getpid(), x, child_pid2);
+    // change x to see if forked properly and
+    x = 7000;
+    Printf(
+        "After x changed: pid:%d checking x(%d) after fork child 2 PID = %d\n",
+        getpid(), x, child_pid2);
+  } else {
+    Printf("This is parent the process with pid %d \n", getpid());
+    Printf(
+        "Before x changed: pid:%d checking x(%d) after fork child 2 PID = %d\n",
+        getpid(), x, child_pid2);
+    // change x to see if forked properly and
+    x = 5555;
+    Printf(
+        "After x changed: pid:%d checking x(%d) after fork child 2 PID = %d\n",
+        getpid(), x, child_pid2);
+  }
+
   // test if child
   if (child_pid == 0) {
     Printf("This is child the process with PID %d \n", getpid());
